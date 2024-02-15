02f469d7ae10996f4da2acd5af38c34f924c9b32
Robby6Strings <rausten93@gmail.com>
2024-02-15 23:15:52 +1300

    ^kaioken version

--- src/App.tsx
+++ src/App.tsx
@@ -11,24 +11,18 @@ export function App() {
         <Route path="/" element={HomePage} />
         <Route path="/boards/:boardId" element={BoardPage} />
       </Router>
-      <Footer />
+      <footer className="fixed bottom-0 w-full px-3 py-2">
+        <div className="text-right">
+          <a
+            href="https://github.com/robby6strings/kaioken-kanban"
+            target="_blank"
+            style="color:crimson"
+            className="inline-flex gap-1"
+          >
+            <GithubIcon />
+          </a>
+        </div>
+      </footer>
     </GlobalProvider>
-  )
-}
-
-function Footer() {
-  return (
-    <footer className="fixed bottom-0 w-full px-3 py-2">
-      <div className="text-right">
-        <a
-          href="https://github.com/robby6strings/kaioken-kanban"
-          target="_blank"
-          style="color:crimson"
-          className="inline-flex gap-1"
-        >
-          <GithubIcon />
-        </a>
-      </div>
-    </footer>
   )
 }
